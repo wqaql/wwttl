fa5c76d1d30fc45559b4535f8ce5bcaa10906cd7
wqaql <lyg58441@gmail.com>
2025-06-19 22:50:17 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -396,7 +396,7 @@ function urlPngToWebp(url: string): string {
  * @param imageUrl
  */
 const getProxyImageUrl = (pathName:string,imageUrl: string): string => {
-  const bfStr = stringToBase64(encodeURIComponent(imageUrl))
+  const bfStr = stringToBase64(imageUrl)
   const str = getRandomAZ2() + bfStr
   let url = `${pathName}/img/${str}`
   url = url.startsWith("/") ? url.substring(1) : url;
